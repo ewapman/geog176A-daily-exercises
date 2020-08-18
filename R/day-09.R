6d11018a167ca33833890d97121187781faf6cb4
EWAPMAN <emily.wap@gmail.com>
2020-08-18 16:48:49 -0700

    commit

--- R/day-09.R
+++ R/day-09.R
@@ -1,13 +1,16 @@
-data = USAboundaries::us_states(resolution = "low") %>%
-  filter(state_name != "Puerto Rico", "Alaska", "Hawaii")
-attributes(data)
+library(sf)
+library(tidyverse)
+data = USAboundaries::us_states() %>%
+  filter(!state_name %in% c("Puerto Rico", "Alaska", "Hawaii"))
+
+usa_geom = data$geometry
+(ca_c_ml = st_combine(usa_geom) %>%
+    st_cast("MULTILINESTRING"))
+
+plot(usa_geom)
 
 
-df = data.frame(name = state_name,
-                geometry = geometry)
-head(df)
-
-
-
-
-
+usa_geom2 = data$geometry
+(ca_u_ml = st_union(usa_geom2)  %>%
+    st_cast("MULTILINESTRING"))
+plot(usa_geom2)
